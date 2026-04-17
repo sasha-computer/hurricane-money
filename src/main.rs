200845f825908e117e76428ed9953b2da7393ad7
Sasha <33594434+sasha-computer@users.noreply.github.com>
2026-04-17 15:10:18 +0100

    reveal commitment

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// src/main.rs
+use openvm::io::{read, reveal_bytes32};
+use openvm_sha2::sha256;
+
+fn main() {
+    let k: u8 = read();
+    let r: u8 = read();
+
+    let c: [u8; 32] = sha256(&[k, r]);
+
+    reveal_bytes32(c)
+}
