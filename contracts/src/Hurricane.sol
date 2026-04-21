086ea58f826753d76d9c7815c83acd86b0ef4faf
Sasha <33594434+sasha-computer@users.noreply.github.com>
2026-04-21 10:44:16 +0100

    progress

--- contracts/src/Hurricane.sol
+++ contracts/src/Hurricane.sol
@@ -6,15 +6,38 @@ import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProo
 
 contract Hurricane {
     using MerkleTree for MerkleTree.Bytes32PushTree;
-
     MerkleTree.Bytes32PushTree private tree;
+    bytes32 public constant HURRICANE_SALT = keccak256("hurricane");
     bytes32 public root;
 
+    event Deposit(
+        bytes32 indexed commitment,
+        uint256 amount,
+        uint256 leafIndex,
+        bytes32 previousRoot,
+        bytes32 newRoot
+    );
+
     constructor() {
-        root = tree.setup(20, keccak256("hurricane"));
+        root = tree.setup(20, HURRICANE_SALT);
     }
 
-    function deposit() public {}
+    function deposit(bytes32 commitment) external payable {
+        require(
+            msg.value == 0.1 ether || msg.value == 1 ether,
+            "Invalid amount: must be 0.1 or 1 ETH"
+        );
+        require(
+            commitment != HURRICANE_SALT,
+            "Invalid commitment: must not be keccak256('hurricane')"
+        );
 
-    function withdraw() public {}
+        (uint256 newLeafIndex, bytes32 newRoot) = tree.push(commitment);
+
+        emit Deposit(commitment, msg.value, newLeafIndex, root, newRoot);
+
+        root = newRoot;
+    }
+
+    // function withdraw() public {}
 }
